2295103533b647e6ae6d09e45e5193936934f157
Joseph Troop <jtroop@srsacquiom.com>
2025-03-01 13:54:31 -0500

    created a repository for all odin project work. Starting with ROR course

---
+++ full_stack_ruby_rails/Projects/ruby_bsics_projects/caesar_cipher.rb
@@ -0,0 +1,29 @@
+#caps: 65 - 90
+#lower: 97 - 122
+
+
+def caesar_cipher(string, shift)
+  begin
+    string.each_char.map do |char|
+      case char.ord
+        when (32) then min = (32 - shift); max = 32
+        when (65..90) then min = 64; max = 90
+        when (97..122) then min = 65; max = 122
+        else raise "#{char} is outside the range of [a-z] or [A-Z]: \nENTRY: #{string}"
+      end
+
+      if (char.ord + shift) > max
+        (min + (shift - (max - char.ord))).chr
+      else
+        (char.ord + shift).chr
+      end
+  end.join
+
+  rescue => e
+    puts e
+  end
+end
+
+
+
+puts caesar_cipher("What a& string!", 5)
