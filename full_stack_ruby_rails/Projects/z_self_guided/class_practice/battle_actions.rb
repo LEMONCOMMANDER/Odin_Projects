793aab4b2106acc56c77b8de9635ba443df17e7b
Joseph Troop <jtroop@srsacquiom.com>
2025-04-01 22:15:28 -0400

    gi 4

--- full_stack_ruby_rails/Projects/z_self_guided/class_practice/battle_actions.rb
+++
@@ -1,7 +0,0 @@
-#module for combat
-#TODO: this module will be inherited by both characters and monsters and will determine what
-# actions can be taken by the characters
-
-module BattleActions
-
-end
